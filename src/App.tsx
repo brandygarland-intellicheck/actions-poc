8ca91a5038a39eb516e6801af9cc820bb8f979ba
brandygarland-intellicheck <bgarland@intellicheck.com>
2024-01-19 12:25:36 -0700

    commit small updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <h1
         >
-          Learn React
-        </a>
+          {process.env.REACT_APP_SAMPLE_ENV}
+        </h1>
       </header>
     </div>
   );
